38cecd9a7d92c8622f5b82a5bbba3f5f1ae0b454
Wyimaginowany <goralczyk.jakub@wp.pl>
2023-10-07 14:29:44 +0200

    test

--- SHOGUN/Assets/test.cs
+++ SHOGUN/Assets/test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test2 : MonoBehaviour
+public class test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
