7728f5adba2f5c34d81270dedbff325ffad52441
karol <karol0704@wp.pl>
2023-10-19 15:07:31 +0200

    adjust volume setting

--- SHOGUN/Assets/Scripts/Menu/Menus/SettingsMenu.cs
+++ SHOGUN/Assets/Scripts/Menu/Menus/SettingsMenu.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text volumeValueText = null;
+    [SerializeField] private Slider volumeSlider = null;
     private Button backButton;
     public void Awake()
     {
@@ -14,5 +17,12 @@ public class SettingsMenu : MonoBehaviour
         {
             MenuManager.setActiveView(MenuManager.mainMenu);
         });
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeValueText.text = volume.ToString("0.0");
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
     }
 }
