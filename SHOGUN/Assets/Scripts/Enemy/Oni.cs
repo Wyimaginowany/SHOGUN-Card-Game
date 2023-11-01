ace3f88752d3f48577f15939a702e5fc0ef2b208
karol <karol0704@wp.pl>
2023-11-01 22:46:01 +0100

    basic Oni + Thief
    enemies missing effects and block

---
+++ SHOGUN/Assets/Scripts/Enemy/Oni.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Oni : EnemyCombat
+{
+    private readonly Random _rnd = new();
+    [SerializeField] private int _strongHitCooldown;
+    [SerializeField] private int _stunAttackCooldown;
+    [SerializeField] private int _buffDamageCooldown;
+    [SerializeField] private int _berserkCooldown;
+    
+    protected override void HandleTurn()
+    {
+        List<string> availableAbilities = new List<string>();
+        if (_strongHitCooldown <= 0) availableAbilities.Add("strongHit");
+        if (_stunAttackCooldown <= 0) availableAbilities.Add("stunAttack");
+        if (_buffDamageCooldown <= 0) availableAbilities.Add("buffDamage");
+        if (_berserkCooldown <= 0) availableAbilities.Add("berserk");
+        
+        _strongHitCooldown--;
+        _stunAttackCooldown--;
+        _buffDamageCooldown--;
+        
+        int selectedIndex = _rnd.Next(availableAbilities.Count);
+        string selectedAbility = availableAbilities[selectedIndex];
+        
+        switch (selectedAbility)
+        {
+            case "strongHit":
+                StrongHit();
+                break;
+            case "stunAttack":
+                StunAttack();
+                break;
+            case "buffDamage":
+                BuffDamage();
+                break;
+            case "berserk":
+                Berserk();
+                break;
+        }
+    }
+    
+    private void StrongHit()
+    {
+        int damage = _rnd.Next(12, 20);
+        playerHealth.TakeDamage(damage);
+
+        _strongHitCooldown = 2;
+    }
+
+    private void StunAttack()
+    {
+        int damage = _rnd.Next(6, 9);
+        playerHealth.TakeDamage(damage);
+        
+        //TODO: stun if player doesnt have enough block to block all damage
+        
+        _stunAttackCooldown = 2;
+    }
+
+    private void BuffDamage()
+    {
+        //TODO: buff dmg by 2 for the rest of the combat (effect stacks)
+        _buffDamageCooldown = 1;
+    }
+
+    private void Berserk()
+    {
+        //TODO: decrease currentHealth by 50%, but increase damage by 30% for the rest of the combat (one time use)
+
+        _berserkCooldown++;
+    }
+}
---
+++ SHOGUN/Assets/Scripts/Enemy/Thief.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Thief : EnemyCombat
+{
+    private readonly Random _rnd = new();
+    [SerializeField] private int _basicAttackCooldown;
+    [SerializeField] private int _comboAttackCooldown;
+    [SerializeField] private int _blockActionCooldown;
+    [SerializeField] private int _buffBlockCooldown;
+    [SerializeField] private int _comboCounter = 1;
+    [SerializeField] private double _damageMultiplier;
+    
+    public enum BeltColor
+    {
+        White=100,
+        Orange=120,
+        Blue=140,
+        Yellow=160,
+        Green=180,
+        Brown=200,
+        Black=220
+    }
+
+    public Thief(BeltColor beltColor)
+    {
+        _damageMultiplier = (double) beltColor / 100;
+    }
+    
+    protected override void HandleTurn()
+    {
+        List<string> availableAbilities = new List<string>();
+        if (_basicAttackCooldown <= 0) availableAbilities.Add("basicAttack");
+        if (_comboAttackCooldown <= 0) availableAbilities.Add("comboAttack");
+        if (_blockActionCooldown <= 0) availableAbilities.Add("blockAction");
+        if (_buffBlockCooldown <= 0) availableAbilities.Add("buffBlock");
+        
+        _basicAttackCooldown--;
+        _comboAttackCooldown--;
+        _blockActionCooldown--;
+        _buffBlockCooldown--;
+        
+        int selectedIndex = _rnd.Next(availableAbilities.Count);
+        string selectedAbility = availableAbilities[selectedIndex];
+        
+        switch (selectedAbility)
+        {
+            case "basicAttack":
+                BasicAttack();
+                break;
+            case "comboAttack":
+                ComboAttack();
+                break;
+            case "blockAction":
+                BlockAction();
+                break;
+            case "buffBlock":
+                BuffBlock();
+                break;
+        }
+    }
+    
+    private void BasicAttack()
+    {
+        int damage = (int) Math.Round(_rnd.Next(3, 5) * _damageMultiplier);
+        playerHealth.TakeDamage(damage);
+    }
+
+    private void ComboAttack()
+    {
+        int damage = (int) Math.Round(2 * _comboCounter * _damageMultiplier);
+        playerHealth.TakeDamage(damage);
+        
+        _comboAttackCooldown = 1;
+    }
+
+    private void BlockAction()
+    {
+        //TODO: give 5 block
+
+        _blockActionCooldown = 1;
+    }
+
+    private void BuffBlock()
+    {
+        //TODO: block multi 150% for the rest of the combat
+
+        _buffBlockCooldown = 5;
+    }
+}
