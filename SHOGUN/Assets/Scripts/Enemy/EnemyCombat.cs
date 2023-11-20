436e3803050883ca500228d25bf45be2e36e68d3
Wyimaginowany <goralczyk.jakub@wp.pl>
2023-11-20 12:31:44 +0100

    major cleanup

--- SHOGUN/Assets/Scripts/Enemy/EnemyCombat.cs
+++ SHOGUN/Assets/Scripts/Enemy/EnemyCombat.cs
@@ -4,6 +4,7 @@ public abstract class EnemyCombat : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] protected int _damage;
+    [SerializeField] protected float _turnTimeAmount = 2f;
 
     [Header("To Attach")]
     [SerializeField] private AnimatorOverrideController _overrideController;
@@ -12,6 +13,8 @@ public abstract class EnemyCombat : MonoBehaviour
     protected Animator _animator;
     private CombatManager _combatManager;
     protected PlayerHealth playerHealth;
+    private float _turnTimer = 0;
+    private bool _isThisEnemyTurn = false;
 
     protected virtual void Start()
     {
@@ -22,19 +25,33 @@ public abstract class EnemyCombat : MonoBehaviour
         _combatManager = (CombatManager)FindObjectOfType(typeof(CombatManager));
         //_playerEffects = ...
     }
-    
+
+    private void Update()
+    {
+        if (!_isThisEnemyTurn) return;
+
+        _turnTimer += Time.deltaTime;
+        if (_turnTimer >= _turnTimeAmount)
+        {
+            EndTurn();
+        }
+
+    }
+
+    private void EndTurn()
+    {
+        _isThisEnemyTurn = false;
+        _turnTimer = 0;
+        _combatManager.NextEnemyTurn();
+    }
+
     public virtual void HandleTurn()
     {
-        
+        _isThisEnemyTurn = true;
     }
 
     private void AttackAnimationEvent()
     {
         playerHealth.TakeDamage(_damage);
-    }
-
-    private void EndAttackAnimationEvent()
-    {
-        _combatManager.NextEnemyTurn();
     }
 }
--- SHOGUN/Assets/Scripts/Enemy/Oni.cs
+++ SHOGUN/Assets/Scripts/Enemy/Oni.cs
@@ -1,91 +1,123 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Oni : EnemyCombat
 {
-    [Header("Max Cooldowns")]
-    [SerializeField] private int _strongHitMaxCD = 2;
-    [SerializeField] private int _stunAttackMaxCD = 2;
-    [SerializeField] private int _buffDamageMaxCD = 2;
-    [SerializeField] private int _berserkMaxCD = 10000;
-    
-    [Header("Moves")]
+    [Header("Attacks")]
+    [SerializeField] private List<EnemyAtack> _oniPossibleAttacks = new List<EnemyAtack>();
+    [Space(5)]
+    [Header("Strong Attack")]
     [SerializeField] private int _strongHitMinDmg = 12;
     [SerializeField] private int _strongHitMaxDmg = 20;
+    [Space(5)]
+    [Header("Stun Attack")]
+    [SerializeField] private int _minShieldToBlockStun = 6;
     [SerializeField] private int _stunAttackMinDmg = 6;
     [SerializeField] private int _stunAttackMaxDmg = 9;
-    [SerializeField] private int _minShieldToBlockStun = 6;
-    
-    private int _strongHitCooldown;
-    private int _stunAttackCooldown;
-    private int _buffDamageCooldown;
-    private int _berserkCooldown;
-    private double _berserkMulti = 1;
-    
+    [Space(5)]
+    [Header("Buff Attack")]
+    [SerializeField] private int _damageBuffPerUse = 2;
+    [Space(5)]
+    [Header("Berserk Attack")]
+    [SerializeField] private int _berserkDamageBuffPercentage = 30;
+    [SerializeField] private int _berserkHealthLossPercentage = 50;
+      
     private EnemyHealth _enemyHealth;
-    
-    
+    private int _currentBerserkMuliplier = 0;
+    private int _currentDamageBuff = 0;
+
+    [System.Serializable]
+    public class EnemyAtack
+    {
+        public OniAttackTypes AttackType;
+        public int AttackPriority = 0;
+        public int AttackCooldown = 2;
+        public int AttackCooldownAtStart = 2;
+        public bool RemoveAfterUsage = false;
+        public string AnimatorTrigger = "";
+        public float AttackDuration = 2f;
+    }
+
+
     protected override void Start()
     {
         base.Start();
         _enemyHealth = GetComponent<EnemyHealth>();
         
     }
+
     public override void HandleTurn()
     {
-        base.HandleTurn();
-        List<string> availableAbilities = new List<string>();
-        //if (_strongHitCooldown <= 0) availableAbilities.Add("strongHit");
-        //if (_stunAttackCooldown <= 0) availableAbilities.Add("stunAttack");
-        //if (_buffDamageCooldown <= 0) availableAbilities.Add("buffDamage");
-        //if (_berserkCooldown <= 0) availableAbilities.Add("berserk");
-        
-        _strongHitCooldown--;
-        _stunAttackCooldown--;
-        _buffDamageCooldown--;
-        
-        //int selectedIndex = Random.Range(0, availableAbilities.Count);
-        //string selectedAbility = availableAbilities[selectedIndex];
+        EnemyAtack oniAttackSelected = GetTurnAttack();
 
-
-        _animator.SetTrigger("attack");
-        /*switch (selectedAbility)
+        switch (oniAttackSelected.AttackType)
         {
-            case "strongHit":
+            case OniAttackTypes.StrongAttack:
                 StrongHit();
                 break;
-            case "stunAttack":
+            case OniAttackTypes.StunAttack:
                 StunAttack();
                 break;
-            case "buffDamage":
+            case OniAttackTypes.BuffAttack:
                 BuffDamage();
                 break;
-            case "berserk":
+            case OniAttackTypes.Berserk:
                 Berserk();
-                break;
-            
-        }*/
+                break;           
+        }
+
+        _turnTimeAmount = oniAttackSelected.AttackDuration;
+        _animator.SetTrigger(oniAttackSelected.AnimatorTrigger);
+        oniAttackSelected.AttackCooldown = oniAttackSelected.AttackCooldownAtStart;
+        if (oniAttackSelected.RemoveAfterUsage) _oniPossibleAttacks.Remove(oniAttackSelected);
+
+        base.HandleTurn();
     }
 
-    public void DefaultAttack()
+    private EnemyAtack GetTurnAttack()
     {
-        playerHealth.TakeDamage(_damage);
+        EnemyAtack chosenAttack = null;
+        int enemyPriority = -1;
+
+        foreach (EnemyAtack enemyAtack in _oniPossibleAttacks)
+        {
+            if (enemyAtack.AttackCooldown > 0)
+            {
+                enemyAtack.AttackCooldown--;
+                continue;
+            }
+                
+
+            if (enemyAtack.AttackPriority > enemyPriority)
+            {
+                enemyPriority = enemyAtack.AttackPriority;
+                chosenAttack = enemyAtack;
+            }
+        }
+
+        chosenAttack.AttackCooldown++;
+        return chosenAttack;
+    }
+
+    private void DealDamageToPlayer(int damage)
+    {
+        damage *= Mathf.RoundToInt((100 + _currentBerserkMuliplier) / 100);
+        playerHealth.TakeDamage(damage + _currentDamageBuff);
     }
 
     private void StrongHit()
     {
-        //_animator.SetTrigger("strongHit");
-        //int damage = (int)(Random.Range(_strongHitMinDmg, _strongHitMaxDmg) * _berserkMulti);
-        //playerHealth.TakeDamage(damage);
+        int damage = UnityEngine.Random.Range(_strongHitMinDmg, _strongHitMaxDmg);
+        DealDamageToPlayer(damage);
 
-        _strongHitCooldown = _strongHitMaxCD;
         Debug.Log("strongHit");
     }
 
     private void StunAttack()
     {
-        //int damage = (int)(Random.Range(_stunAttackMinDmg, _stunAttackMaxDmg) * _berserkMulti);
-        //playerHealth.TakeDamage(damage);
+        int damage = UnityEngine.Random.Range(_stunAttackMinDmg, _stunAttackMaxDmg);
+        DealDamageToPlayer(damage);
 
         if (playerHealth.GetPlayerShield() < _minShieldToBlockStun)
         {
@@ -93,23 +125,23 @@ public class Oni : EnemyCombat
             Debug.Log("stunPlayer");
         }
         
-        _stunAttackCooldown = _stunAttackMaxCD;
         Debug.Log("stunAttack");
     }
 
     private void BuffDamage()
     {
         //TODO: buff dmg by 2 for the rest of the combat (effect stacks)
-        _buffDamageCooldown = _buffDamageMaxCD;
+        _currentDamageBuff += _damageBuffPerUse;
         Debug.Log("buffDamage");
     }
 
     private void Berserk()
     {
         //TODO: decrease currentHealth by 50%, but increase damage by 30% for the rest of the combat (one time use)
-        _enemyHealth.SetCurrentHealth(_enemyHealth.GetCurrentHealth()/2);
-        _berserkMulti = 1.3;
-        _berserkCooldown = _berserkMaxCD;
-        Debug.Log("berserj");
+        _enemyHealth.SetCurrentHealth(_enemyHealth.GetCurrentHealth()/(100 / _berserkHealthLossPercentage));
+        _currentBerserkMuliplier = _berserkDamageBuffPercentage;
+        Debug.Log("berserk");
     }
 }
+
+public enum OniAttackTypes { StrongAttack, StunAttack, BuffAttack, Berserk }
