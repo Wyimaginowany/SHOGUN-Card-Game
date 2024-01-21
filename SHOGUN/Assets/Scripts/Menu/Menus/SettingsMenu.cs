d078cc9f191bc5ded4f1c678d753bcc7efbf3e05
Wyimaginowany <goralczyk.jakub@wp.pl>
2024-01-21 12:46:00 +0100

    sounds settings

--- SHOGUN/Assets/Scripts/Menu/Menus/SettingsMenu.cs
+++ SHOGUN/Assets/Scripts/Menu/Menus/SettingsMenu.cs
@@ -3,25 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    /*[SerializeField] private TMP_Text volumeValueText = null;
-    private Button _backButton;
-    public void Awake()
-    {
-        _backButton = GameObject.Find("BackButton").gameObject.GetComponent<Button>();
+    [SerializeField] private AudioMixer _audioMixer;
 
-        _backButton.onClick.AddListener(() =>
-        {
-            MenuManager.SetActiveView(MenuManager.MainMenu);
-        });
+    public void SetMusicVolume(float volume)
+    {
+        _audioMixer.SetFloat("MusicVolume", volume);
     }
-
-    public void SetVolume(float volume)
+    public void SetSoundsVolume(float volume)
     {
-        AudioListener.volume = volume;
-        volumeValueText.text = volume.ToString("0.0");
-        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-    }*/
+        _audioMixer.SetFloat("SoundVolume", volume);
+    }
 }
